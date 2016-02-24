831171ea41ca7c61ae176e1a97cc08efb26e29cf
Owner <a246530@gmail.com>
2016-02-24 05:25:40 -0500

    added ability to onhover the text
    Added ability to have on hover text.
    Updated qar with changes.

--- Extensions/Objects/materialIcons/script.js
+++ Extensions/Objects/materialIcons/script.js
@@ -9,17 +9,35 @@ Qv.AddExtension("materialIcons",
 		 var size=this.Layout.Text1.text ? this.Layout.Text1.text : '24';
 		 var color=this.Layout.Text2.text ? this.Layout.Text2.text : '#bbbbbb';
 		 var Text=this.Layout.Text3.text ? this.Layout.Text3.text : ' ';
-		  var fontSize=this.Layout.Text4.text ? this.Layout.Text4.text : '24';
+		 var OnHoverEnable=this.Layout.Text4.text ? this.Layout.Text4.text : 0;
+		  var fontSize=this.Layout.Text5.text ? this.Layout.Text5.text : '24';
 		 var html="";
 		 $(_this.Element).empty();
 		 RenderExtension();
-
+		 
+		 
+		 if(OnHoverEnable==1)
+		 {
+			 $("#materialTable").hover(function(){
+				$( "#iconText" ).fadeIn( "slow", function() {
+				  });
+			 }, function () {
+				  $( "#iconText" ).fadeOut( "slow", function() {
+				  });
+			});
+		 }
 		function RenderExtension() {
 			html="";
 			$(_this.Element).empty();
-			html+='<table><tr><td><i style="font-size:'+size+'px;padding:2px;color:'+color+'" class="material-icons">'+icon+'</i></td><td><p style="font-size:'+fontSize+'px">'+Text+'</p></td></tr></table>';
+			if(OnHoverEnable==1){
+				html+='<table id="materialTable"><tr><td><i style="font-size:'+size+'px;padding:2px;color:'+color+'" class="material-icons">'+icon+'</i></td><td><p id="iconText" style="font-size:'+fontSize+'px;display:none;">'+Text+'</p></td></tr></table>';
+			}else{
+				html+='<table id="materialTable"><tr><td><i style="font-size:'+size+'px;padding:2px;color:'+color+'" class="material-icons">'+icon+'</i></td><td><p id="iconText" style="font-size:'+fontSize+'px;">'+Text+'</p></td></tr></table>';
+			}
 			_this.Element.innerHTML=html;
 			html="";
 		}
+		
  });
+
  
